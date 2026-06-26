bde2ef3daa72ddb4ef02fee6138b3088f0ee3747
Keerthireddy <puthalapattukeerthireddy9@gmail.com>
2026-06-26 17:45:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-26-06"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
